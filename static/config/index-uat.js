ccfeeedf9bbfd902e8d1a0f978373c0de53c4a52
qjq <qjq>
2021-04-15 16:19:44 +0800

    tcj提交

--- static/config/index-uat.js
+++ static/config/index-uat.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:12150/renren-fast';
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:1215/renren-fast';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
